e7c67f5a3c93abb900cae112d59a8a56b48e8599
InkChan <1986140521@qq.com>
2022-02-10 13:55:40 +0800

    Update sgemm.cu

--- Chapter02/02_memory_overview/01_sgemm/sgemm.cu
+++ Chapter02/02_memory_overview/01_sgemm/sgemm.cu
@@ -28,7 +28,7 @@ sgemm_gpu_kernel(const float *A, const float *B, float *C, int N, int M, int K, 
 
 	float sum = 0.f;
 	for (int i = 0; i < K; ++i) {
-		sum += A[row * K + i] * B[i * K + col];
+		sum += A[row * K + i] * B[i * M + col];
 	}
 	
 	C[row * M + col] = alpha * sum + beta * C[row * M + col];
